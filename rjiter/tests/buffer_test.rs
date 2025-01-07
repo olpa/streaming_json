1f66fbdba7f1d3999260b145b8f5013e560c3198
Oleg Parashchenko <olpa@uucode.com>
2025-01-07 04:06:25 +0100

    Test for one read.

--- rjiter/tests/buffer_test.rs
+++ rjiter/tests/buffer_test.rs
@@ -28,3 +28,17 @@ fn test_skip_spaces_from_non_zero_pos() {
     // assert
     assert_eq!(&buffer.buf[..buffer.n_bytes], b"  abc");
 }
+
+#[test]
+fn test_skip_spaces_with_one_read() {
+    let input = "     abc".as_bytes();
+    let mut reader = Cursor::new(input);
+    let mut buf = [0u8; 4];
+    let mut buffer = Buffer::new(&mut reader, &mut buf);
+
+    // act
+    buffer.skip_spaces(0);
+
+    // assert
+    assert_eq!(&buffer.buf[..buffer.n_bytes], b"abc");
+}
