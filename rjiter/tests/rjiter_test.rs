80e02bd56933671130f1dd3e7c451132b1669e2b
Oleg Parashchenko <olpa@uucode.com>
2025-01-10 18:43:35 +0100

    Update the test for write small amount of bytes.

--- rjiter/tests/rjiter_test.rs
+++ rjiter/tests/rjiter_test.rs
@@ -43,6 +43,20 @@ fn skip_spaces() {
 }
 
 #[test]
+fn pass_through_small_bytes() {
+    let input = r#""small text""#;
+    let mut buffer = [0u8; 100];
+    let mut reader = Cursor::new(input.as_bytes());
+    let mut writer = Vec::new();
+    let mut rjiter = RJiter::new(&mut reader, &mut buffer);
+
+    let wb = rjiter.write_long_bytes(&mut writer);
+    wb.unwrap();
+
+    assert_eq!(writer, "small text".as_bytes());
+}
+
+#[test]
 fn pass_through_long_bytes() {
     let input = r#""very very very long string""#;
     let mut buffer = [0u8; 5];
@@ -55,27 +69,6 @@ fn pass_through_long_bytes() {
     wb.unwrap();
 
     assert_eq!(writer, "very very very long string".as_bytes());
-}
-
-#[test]
-fn pass_through_small_bytes() {
-    let input = r#"{ "text": "small" }"#;
-    let mut buffer = [0u8; 100];
-    let mut reader = Cursor::new(input.as_bytes());
-    let mut writer = Vec::new();
-
-    let mut rjiter = RJiter::new(&mut reader, &mut buffer);
-
-    // Consume object start
-    assert_eq!(rjiter.next_object().unwrap(), Some("text"));
-    rjiter.feed();
-    assert_eq!(rjiter.peek().unwrap(), Peek::String);
-
-    // Consume the string value
-    let wb = rjiter.write_long_bytes(&mut writer);
-    wb.unwrap();
-
-    assert_eq!(writer, "small".as_bytes());
 }
 
 #[test]
