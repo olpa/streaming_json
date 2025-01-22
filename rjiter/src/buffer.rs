c0d1b68d2931b7a929493f31f762b22dab7af8d9
Oleg Parashchenko <olpa@uucode.com>
2025-01-22 05:19:42 +0100

    Shift calculations: check that can slice.

--- rjiter/src/buffer.rs
+++ rjiter/src/buffer.rs
@@ -37,20 +37,23 @@ impl<'buf> Buffer<'buf> {
     }
 
     pub fn shift_buffer(&mut self, to_pos: usize, from_pos: usize) {
-        for ch in &self.buf[to_pos..min(from_pos, self.n_bytes)] {
-            if *ch == b'\n' {
-                self.pos_shifted.line += 1;
-                self.pos_shifted.column = 0;
-            } else {
-                self.pos_shifted.column += 1;
+        let safe_from_pos = min(from_pos, self.n_bytes);
+        if to_pos < safe_from_pos {
+            for ch in &self.buf[to_pos..safe_from_pos] {
+                if *ch == b'\n' {
+                    self.pos_shifted.line += 1;
+                    self.pos_shifted.column = 0;
+                } else {
+                    self.pos_shifted.column += 1;
+                }
             }
         }
+
         if from_pos > to_pos && to_pos < self.n_bytes {
             if from_pos < self.n_bytes {
                 self.buf.copy_within(from_pos..self.n_bytes, to_pos);
             }
-            let from_pos = min(from_pos, self.n_bytes);
-            let n_shifted_out = from_pos - to_pos;
+            let n_shifted_out = safe_from_pos - to_pos;
             self.n_bytes -= n_shifted_out;
             self.n_shifted_out += n_shifted_out;
         }
