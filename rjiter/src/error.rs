7e4d5d9c33ebc84ba6b863eb5e33a3a0af3b6727
Oleg Parashchenko <olpa@uucode.com>
2025-01-23 04:18:25 +0100

    Code for Display.

--- rjiter/src/error.rs
+++ rjiter/src/error.rs
@@ -13,6 +13,7 @@ pub enum ErrorType {
     },
     IoError(std::io::Error),
 }
+
 impl std::fmt::Display for ErrorType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -29,6 +30,12 @@ impl std::fmt::Display for ErrorType {
 pub struct Error {
     pub error_type: ErrorType,
     pub index: usize,
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} at index {}", self.error_type, self.index)
+    }
 }
 
 impl Error {
