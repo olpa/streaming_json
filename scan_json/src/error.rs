0db9e1b5295a68ab6e7bf9e6862653173bdfae71
Oleg Parashchenko <olpa@uucode.com>
2025-02-06 05:48:59 +0100

    Show error handling in README.

--- scan_json/src/error.rs
+++ scan_json/src/error.rs
@@ -8,6 +8,8 @@ pub enum Error {
     ActionError(Box<dyn std::error::Error>),
 }
 
+impl std::error::Error for Error {}
+
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
