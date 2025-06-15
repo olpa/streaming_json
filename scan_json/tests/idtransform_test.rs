3c2dec4c07d76fa48c176abda39416ea5f9da082
Oleg Parashchenko <olpa@uucode.com>
2025-06-15 04:00:53 +0200

    Add passing test: array in array.

--- scan_json/tests/idtransform_test.rs
+++ scan_json/tests/idtransform_test.rs
@@ -116,6 +116,35 @@ fn idt_object_in_object() {
 }
 
 #[test]
+fn idt_array_in_array() {
+    let input = r#"
+        [
+            [1, 2, 3],
+            ["a", "b", "c"],
+            [true, false, null]
+        ]
+    "#;
+
+    let mut reader = input.as_bytes();
+    let mut buffer = vec![0u8; 16];
+    let rjiter = RJiter::new(&mut reader, &mut buffer);
+    let rjiter_cell = RefCell::new(rjiter);
+    let mut writer = Vec::new();
+
+    //
+    // Apply and assert
+    //
+    idtransform(&rjiter_cell, &mut writer).unwrap();
+    let output = String::from_utf8(writer).unwrap();
+    let expected = input.split_whitespace().collect::<Vec<&str>>().join("");
+    assert_eq!(
+        output.trim(),
+        expected,
+        "Output should match input after idtransform. Output: {output}"
+    );
+}
+
+#[test]
 fn idt_deeply_nested() {
     let input = r#"
         {
